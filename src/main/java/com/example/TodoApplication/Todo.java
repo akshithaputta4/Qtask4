2e5bde7fcc8bf47877ead39743ffa13abe6b7450
Putta Akshitha <puttaakshitha3399@Gmail.com>
2024-12-30 16:26:36 +0530

    commit

--- src/main/java/com/example/TodoApplication/Todo.java
+++ src/main/java/com/example/TodoApplication/Todo.java
@@ -23,27 +23,5 @@ public class Todo {
     private boolean completed;
 
 
-    public String getText() {
-        return text;
-    }
 
-    public void setText(String text) {
-        this.text = text;
-    }
-
-    public boolean isCompleted() {
-        return completed;
-    }
-
-    public void setCompleted(boolean completed) {
-        this.completed = completed;
-    }
-
-    public String getId() {
-        return id;
-    }
-
-    public void setId(String id) {
-        this.id = id;
-    }
 }
